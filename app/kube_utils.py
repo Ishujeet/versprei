e4235140b34bc186ee8b8c36a2a433253c4a1795
Ishujeet Panjeta <ipanjeta@mafcarrefour.com>
2023-07-24 00:46:13 +0400

    first commit

---
+++ app/kube_utils.py
@@ -0,0 +1,66 @@
+import logging
+from pprint import pprint
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+config.load_incluster_config()
+
+PodDistributorGroup = "versprei.versprei.io"
+PodDistributorVersion = "v1beta1"
+PodDistributorPlural = "poddistributors"
+
+
+def get_pod_distribution(deployment_name):
+    api_instance = client.CustomObjectsApi()
+    
+    # Get distribution spec for deployment
+    try:
+        api_response = api_instance.get_namespaced_custom_object(
+        group=PodDistributorGroup, version=PodDistributorVersion, namespace="default", plural=PodDistributorPlural, name=deployment_name)
+        pod_distribution = []
+        for d in api_response['spec']['distribution']:
+            for k, v in d['nodeLabel'].items():
+                nodeLabel = f"{k}={v}"
+            pod_distribution.append({"nodeLabel": nodeLabel, 'weight': d['weight']})
+        return pod_distribution
+    except ApiException as e:
+        if e.status == 404:
+            return "Not Found"
+        if e.status == 403:
+            return "Not Found"
+        logger.exception("Exception when calling CustomObjectsApi->get_namespaced_custom_object: %s\n" % e)
+        return "Not Found"
+    
+def get_pods_for_deployment(deployment_name):
+    api_instance = client.CoreV1Api()
+    label_selector = f'app={deployment_name}'
+    
+    # Get pods for the deployment
+    try:
+        api_response = api_instance.list_namespaced_pod(
+            namespace="default", label_selector=label_selector, watch=False)
+    except ApiException as e:
+        if e.status == 404:
+            logger.info(f"Pods not found for {deployment_name}")
+            return []
+        if e.status == 403:
+            logger.info(f"Access denied to get Pods for {deployment_name}")
+            return []
+        logger.exception(
+            "Exception when calling AppsV1Api->list_namespaced_pod: %s\n" % e)
+        return []
+    
+    pods = api_response.to_dict()['items']
+    # If no pods running retrun not found
+    if len(pods) == 0:
+        return []
+    
+    list_of_pods = []
+    for pod in pods:
+        for k, v in pod["spec"]["node_selector"].items():
+            pod_info = {"name": pod['metadata']['name'], "nodeSelector": f'{k}={v}'}
+        list_of_pods.append(pod_info)
+    return list_of_pods
---
+++ app/utils.py
@@ -0,0 +1,82 @@
+import logging
+import os
+from .kube_utils import get_pod_distribution, get_pods_for_deployment
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+DistributionSpec = [
+    {
+        "nodeLabel": "type=spotnew",
+        "weight": 50
+    },
+    {
+        "nodeLabel": "type=spotnew2",
+        "weight": 50
+    }
+]
+
+def get_percentage_of_running_pods_by_node_selector(list_of_running_pods, nodelabels, spread_node_label):
+    total_running_pods = 0
+    for pod in list_of_running_pods:
+        if pod["nodeSelector"] in nodelabels:
+            total_running_pods += 1
+        else:
+            continue
+    pod_count = 0
+    for pod in list_of_running_pods:
+        if spread_node_label == pod["nodeSelector"]:
+            pod_count += 1
+    try:
+        percentage = (pod_count/total_running_pods)*100
+    except ZeroDivisionError:
+        percentage = 0
+    return percentage
+    
+
+
+def get_node_selector_patch(deployment_name):
+    
+    # Setting default for testing
+    if os.getenv('ENV') == "dev":
+        distribution_spec = DistributionSpec
+    else:
+        distribution_spec = get_pod_distribution(deployment_name=deployment_name)
+
+    # Distribution spec not found then Ignore patching
+    if os.getenv('ENV') != "dev" and distribution_spec == "Not Found":
+        return None
+    list_of_running_pods = get_pods_for_deployment(
+        deployment_name=deployment_name)
+    
+    # If running pods not found for deployment
+    # either first deployment or not pods running for deployment
+    # then just patch first pod for distribution spec node label
+    if list_of_running_pods == []:
+        for d in distribution_spec:
+            key = d["nodeLabel"].split("=")[0]
+            value = d["nodeLabel"].split("=")[1]
+            node_selector = {key: value}
+            logger.info(f"Patching deployment {deployment_name} with nodeSelector {node_selector}")
+            return node_selector
+    else:
+        nodelables = []
+        for d in distribution_spec:
+            nodelables.append(d["nodeLabel"])
+        for d in distribution_spec:
+            # Get percentage of pods for nodelabel 
+            weight = get_percentage_of_running_pods_by_node_selector(
+                list_of_running_pods=list_of_running_pods, nodelabels=nodelables, spread_node_label=d["nodeLabel"])
+            logger.info(f"Current weight of pods with node label {d['nodeLabel']} is {weight}")
+            # If weight of the nodelabel is greater than
+            # wieght set in distribution spec nodelabel,
+            # don't patch it 
+            if weight > d["weight"]:
+                logger.info(f"Current weight {weight} is greater than distribution weight {d['weight']}")
+                continue
+            else:
+                key = d["nodeLabel"].split("=")[0]
+                value = d["nodeLabel"].split("=")[1]
+                node_selector = {key: value}
+                logger.info(f"Patching deployment {deployment_name} with nodeSelector {node_selector}")
+                return node_selector

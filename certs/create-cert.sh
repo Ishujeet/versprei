95d5c89390ccad9c6d244355a172df6ec6229893
Ishujeet Panjeta <ipanjeta@mafcarrefour.com>
2023-08-02 10:03:54 +0400

    improved script

--- certs/create-cert.sh
+++ certs/create-cert.sh
@@ -1,21 +1,52 @@
 #! /bin/sh
 set -uo errexit
 
-export APP="spread-webhook-service"
-export NAMESPACE="default"
-export CSR_NAME="${APP}.${NAMESPACE}.svc"
+APP=${1:=spread-webhook-service}
+NAMESPACE=${2:=default}
+CSR_NAME="${APP}.${NAMESPACE}.svc"
 
-brew install cfssl
+HAS_WGET="$(type "wget" &> /dev/null && echo true || echo false)"
+HAS_CFSSL="$(type "CFSSL" &> /dev/null && echo true || echo false)"
+HAS_BREW="$(type "brew" &> /dev/null && echo true || echo false)"
 
-# wget -q --show-progress --https-only --timestamping \
-#   https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 \
-#   https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-# chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
-# sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
-# sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+# initOS discovers the operating system for this system.
+initOS() {
+  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+  case "$OS" in
+    # Minimalist GNU for Windows
+    mingw*|cygwin*) OS='windows';;
+  esac
+}
 
 
-### Create CA cert
+if [ "${HAS_CURL}" != "true" ]; then
+  echo "Curl is required"
+  exit 1
+fi
+
+if [ "${OS}" == "darwin" ]; then
+  if [ "${HAS_BREW}" == "true" ]; then
+    if [ "${HAS_CFSSL}" != "true" ]; then
+      echo "Installing cfssl...."
+      brew install cfssl
+    fi
+  else
+    echo "HomeBrew is required"
+  fi
+else
+  if [ "${HAS_CFSSL}" != "true" ]; then
+    echo "Installing cfssl...."
+    wget -q --show-progress --https-only --timestamping \
+      https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 \
+      https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+    chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
+    sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
+    sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+  fi
+fi
+
+echo "Creating CA config json.........."
 
 cat > ca-config.json <<EOF
 {
@@ -32,7 +63,7 @@ cat > ca-config.json <<EOF
   }
 }
 EOF
-
+echo "Creating CA CSR json.........."
 cat > ca-csr.json <<EOF
 {
   "CN": "Kubernetes",
@@ -51,11 +82,11 @@ cat > ca-csr.json <<EOF
   ]
 }
 EOF
-
+echo "Generating CA cert.........."
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 
-### Create Server Cert
 
+echo "Creating SERVER CSR json.........."
 cat > server-csr.json <<EOF
 {
   "CN": "admission",
@@ -74,16 +105,13 @@ cat > server-csr.json <<EOF
   ]
 }
 EOF
+echo "Generating SERVER cert and key.........."
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -hostname=${CSR_NAME} -profile=server server-csr.json | cfssljson -bare server
 
-cfssl gencert \
-  -ca=ca.pem \
-  -ca-key=ca-key.pem \
-  -config=ca-config.json \
-  -hostname=spread-webhook-service.default.svc\
-  -profile=server \
-  server-csr.json | cfssljson -bare server
 
-cat ca.pem | base64 | tr -d '\n'
-
+echo "Converting .pem key & cert to .key and .crt.........."
 openssl x509 -in server.pem -out server.crt
 openssl pkey -in server-key.pem -out server-key.key
+
+echo "Replace CA bundle in MutatingWebhookConfiguration with below one...."
+cat ca.pem | base64 | tr -d '\n'
